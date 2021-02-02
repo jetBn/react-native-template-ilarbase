38dd43803200a60430eff03709b13f524700607a
cwd <1022441695@qq.com>
2021-02-02 10:10:17 +0800

    update template project

---
+++ template/index.js
@@ -0,0 +1,11 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './src';
+
+const APP_NAME = 'ProjectName';
+
+AppRegistry.registerComponent(APP_NAME, () => App);
+
